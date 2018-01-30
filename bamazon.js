ec433f382218e14509a432bc8d54463d9de300d1
Brandon Shapiro <shapyro@Brandons-MacBook-Pro.local>
2018-01-29 20:22:44 -0600

    step 1 complete

--- bamazon.js
+++ bamazon.js
@@ -2,6 +2,9 @@ var mysql = require('mysql');
 var inquirer = require('inquirer');
 const cTable = require('console.table');
 
+var choice;
+var amt;
+
 var connection = mysql.createConnection({
   host     : 'localhost',
   port: '8889',
@@ -25,13 +28,12 @@ connection.connect(function(err) {
         }
       ]);
     });
-    shop();
+    pick();
   })
-  // run the start function after the connection is made to prompt the user
   
 });
 
-function shop() {
+function pick() {
   // do some other stuff
   connection.query("SELECT * FROM bamazon.products", function(err, res){
   if (err) throw err;
@@ -45,10 +47,59 @@ function shop() {
     .then(function(answer) {
       var chosenItemArray = res.filter(item => item.product_name === answer.pick);
       var chosenItem = chosenItemArray[0];
+      choice = chosenItem.product_name;
+      amt = chosenItem.price;
       // console.log(chosenItemArray)
       console.log('chosen: ' + chosenItem.product_name)
       console.log('answer: ' + answer.pick)
       // if (res.i)
+      shop();
     });
+
   });
+  
+}
+
+function shop() {
+  connection.query('SELECT * FROM bamazon.products WHERE ?',  
+  {
+    product_name: choice
+  },
+  function(err, res){
+    if (err) throw err;
+
+    inquirer
+      .prompt([
+        {
+          name: 'qty',
+          type: 'input',
+          message: `How many items would you like to purchase?`
+        }
+      ])
+      .then(function(answer) {
+        if (res[0].stock_qty < parseInt(answer.qty)) {
+          console.log(`we only have ${res[0].stock_qty} left in stock`)
+        } else {
+          console.log('processing purchase')
+          var diff = res[0].stock_qty - answer.qty;
+          var updateQuery = 'UPDATE bamazon.products SET ? WHERE ?'
+          connection.query(updateQuery, 
+            [
+              {
+                stock_qty: diff
+              },
+              {
+                product_name: choice
+              }
+            ],
+            function(err){
+              if (err) throw err;
+              console.log("purchase complete")
+              console.log(`that will be $${amt} please`);
+            }
+          );
+        }
+      })
+  })
+
 }
