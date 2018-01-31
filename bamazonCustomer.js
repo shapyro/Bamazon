4d1139f35f44f8415e48eb3cea4a480147a4b3ee
Brandon Shapiro <shapyro@Brandons-MacBook-Pro.local>
2018-01-31 00:59:42 -0600

    Blind commit

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,7 +3,6 @@ var inquirer = require('inquirer');
 const cTable = require('console.table');
 
 var choice;
-var amt;
 
 var connection = mysql.createConnection({
   host     : 'localhost',
@@ -38,7 +37,6 @@ connection.connect(function(err) {
 });
 
 function pick() {
-  // do some other stuff
   connection.query("SELECT * FROM bamazon.products", function(err, res){
   if (err) throw err;
 
@@ -52,7 +50,6 @@ function pick() {
       var chosenItemArray = res.filter(item => item.product_name === answer.pick);
       var chosenItem = chosenItemArray[0];
       choice = chosenItem.product_name;
-      amt = chosenItem.price;
       shop();
     });
 
@@ -77,6 +74,7 @@ function shop() {
         }
       ])
       .then(function(answer) {
+        var purchaseTotal = answer.qty * res[0].price;
         if (res[0].stock_qty < parseInt(answer.qty)) {
           console.log(`we only have ${res[0].stock_qty} left in stock`)
         } else {
@@ -95,7 +93,7 @@ function shop() {
             function(err){
               if (err) throw err;
               console.log("purchase complete")
-              console.log(`That will be $${amt} please`);
+              console.log(`That will be $${purchaseTotal} please`);
             }
           );
         }
